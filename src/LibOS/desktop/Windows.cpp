e4229ea1b63718b9e3b8a878292d370edb286707
Silentboy <mastjohnny@gmail.com>
2025-10-28 12:08:22 +0100

    Update README badge to reflect new repository and workflow

--- src/LibOS/desktop/Windows.cpp
+++ src/LibOS/desktop/Windows.cpp
@@ -4,8 +4,25 @@ using namespace LibOS::Desktop;
 
 #if PLATFORM_WINDOWS
 
+#include <windows.h>
+#include <iostream>
+
 std::optional<WindowInfo> Windows::GetActiveWindow() {
-    return Base::GetActiveWindow();
+    HWND hwnd = GetForegroundWindow();
+
+    // Buffer voor de venstertitel
+    char windowTitle[256];
+    GetWindowTextA(hwnd, windowTitle, sizeof(windowTitle));
+
+    if (strlen(windowTitle)) {
+        WindowInfo result = WindowInfo();
+        result.title = std::string(windowTitle);
+
+        return result;
+    }
+    // return builder.ToString().Contains(windowTitle, StringComparison.OrdinalIgnoreCase);
+
+    return std::nullopt;
 }
 
 #endif
