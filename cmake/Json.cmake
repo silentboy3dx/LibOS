630d590d8ad8466b39ad503e5a48604372e441a4
Johnny Mast <mastjohnny@gmail.com>
2026-04-05 17:53:13 +0200

    Streamlined nlohmann_json integration by replacing FetchContent with find_package and updating related CMake configurations.

--- cmake/Json.cmake
+++ cmake/Json.cmake
@@ -1,12 +1,6 @@
-include(FetchContent)
+find_package(nlohmann_json CONFIG REQUIRED)
 
-FetchContent_Declare(
-        nlohmann_json
-        GIT_REPOSITORY https://github.com/nlohmann/json.git
-        GIT_TAG v3.11.3
+target_link_libraries(LibOS
+        PUBLIC
+        nlohmann_json::nlohmann_json
 )
-
-FetchContent_MakeAvailable(nlohmann_json)
-set(nlohmann_json_DIR "${nlohmann_json_SOURCE_DIR}/cmake")
-
-target_link_libraries(LibOS PRIVATE nlohmann_json::nlohmann_json)
