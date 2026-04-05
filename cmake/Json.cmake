bad60dbae22ba24ca5a4300f99c6ea0b4f1505cc
Johnny Mast <mastjohnny@gmail.com>
2026-04-05 17:18:03 +0200

    Refactored CMake to streamline nlohmann_json integration, updated dependency handling, and adjusted target configurations

--- cmake/Json.cmake
+++ cmake/Json.cmake
@@ -1,14 +1,12 @@
-if(NOT TARGET nlohmann_json::nlohmann_json)
-    include(FetchContent)
+include(FetchContent)
 
-    FetchContent_Declare(
-            nlohmann_json
-            GIT_REPOSITORY https://github.com/nlohmann/json.git
-            GIT_TAG v3.11.3
-    )
+FetchContent_Declare(
+        nlohmann_json
+        GIT_REPOSITORY https://github.com/nlohmann/json.git
+        GIT_TAG v3.11.3
+)
 
-    FetchContent_MakeAvailable(nlohmann_json)
-endif()
+FetchContent_MakeAvailable(nlohmann_json)
+set(nlohmann_json_DIR "${nlohmann_json_SOURCE_DIR}/cmake")
 
-
-target_link_libraries(${PROJECT_NAME} PUBLIC nlohmann_json::nlohmann_json)
+target_link_libraries(LibOS PRIVATE nlohmann_json::nlohmann_json)
--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -3,5 +3,5 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux" )
 
     find_package(X11 REQUIRED)
 
-    target_link_libraries(${PROJECT_NAME} PRIVATE X11 Xrandr)
+    target_link_libraries(LibOS PRIVATE X11 Xrandr)
 endif()
