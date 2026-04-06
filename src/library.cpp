0ad17a3a1630019bd1d89bf5bda2dbbbbcc082b2
Johnny Mast <mastjohnny@gmail.com>
2026-04-06 11:45:23 +0200

    Simplify `DesktopInfoinstance` and `SystemInfoinstance` method return calls in `LibOS`.

--- src/library.cpp
+++ src/library.cpp
@@ -4,17 +4,14 @@ using namespace LibOS::Desktop;
 
 namespace LibOS {
     std::optional<WindowInfo> GetActiveWindow() {
-        auto result = DesktopInfoinstance();
-        return result.GetActiveWindow();
+        return DesktopInfoinstance().GetActiveWindow();
     }
 
     Resolution GetScreenResolution() {
-        auto result = DesktopInfoinstance();
-        return result.GetScreenResolution();
+        return DesktopInfoinstance().GetScreenResolution();
     }
 
     SystemInfo GetSystemInfo() {
-        auto result = SystemInfoinstance();
-        return result.GetSystemInfo();
+        return SystemInfoinstance().GetSystemInfo();
     }
 }
